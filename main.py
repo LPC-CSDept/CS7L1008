71e16493ae83cf29105e5470a056e55f3d8a0c21
Kyu Lee <kwooler@gmail.com>
2023-06-09 21:35:58 -0700

    L1008

--- main.py
+++ main.py
@@ -1,40 +1,14 @@
 
 class Person:
-    def __init__(self, name, addr, tel):
-        self.name = name
-        self.addr = addr
-        self.tel = tel
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, name):
-        self._name = name
-
-    @property
-    def addr(self):
-        return self._addr
-
-    @addr.setter
-    def addr(self, addr):
-        self._addr = addr
-
-    @property
-    def tel(self):
-        return self._tel
-
-    @tel.setter
-    def tel(self, tel):
-        self._tel = tel
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
 
 
 class Student(Person):
     numofStudent = 0
-    ########################################
-    # Code your program here
-    ########################################
 
     def __str__(self):
         strval = f'Name: {self._name:>10} \t Address: {self.addr} \t Tel: {self.tel}'
@@ -42,6 +16,11 @@ class Student(Person):
         for i in range(len(self._scores)):
             strval += str(self._scores[i]) + '\t'
         return strval
+    """
+    ########################################
+    Code Your Program here
+    ########################################
+    """
 
 
 def main():
@@ -51,13 +30,13 @@ def main():
 
     s1 = Student('James', '123 Ave', '555-555-1234', 1001, sc1)
     s2 = Student('Mary', '456 Ave', '555-555-9999', 1002, sc2)
-    print(s1)
+    print(s1)   # call the function __str__()
     print(s2)
-    print(f'Total number of students: {Student.numofStudent}')
-    print(f'Difference between s1 and s2 scores: {s1-s2}')
+    print(f'Total number of students: {Student.numofStudent}')  # it should be 2
+    print(f'Difference between s1 and s2 scores: {s1-s2}')      # Expected value 10.0
 
-    print(s1 > s2)
-    print(s1 < s2)
+    print(s1 > s2)  # True
+    print(s1 < s2)  # False
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -1,16 +1,7 @@
 import main
-import io
-import sys
-import re
-import math
-import types
 
 
 def test_main_1():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    # datastr = 'Kim \n 100 80 70 60\n Bill \n 100 90 80 60 \n Mary \n 90 80 70 100'
-    # sys.stdin = io.StringIO(datastr)
 
     sc1 = [100, 100, 100]
     sc2 = [90, 90, 90]
@@ -20,24 +11,25 @@ def test_main_1():
     print(s1)
     print(s2)
     print(f'Total number of students: {main.Student.numofStudent}')
-    print(f'Difference between s1 and s2 scores: {s1-s2}')
 
-    print(s1 > s2)
-    print(s1 < s2)
+    assert s1.name == 'James'
+    assert s2.name == 'Mary'
+    assert s1.scores == [100, 100, 100]
+    assert s2.scores == [90, 90, 90]
 
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
 
+def test_main_2():
+    sc1 = [100, 100, 100]
+    sc2 = [90, 90, 90]
+
+    s1 = main.Student('James', '123 Ave', '555-555-1234', 1001, sc1)
+    s2 = main.Student('Mary', '456 Ave', '555-555-9999', 1002, sc2)
+
+    assert callable(s1.__str__) == True
+    assert callable(s1.__sub__) == True
+    assert callable(s1.__gt__) == True
+    assert callable(s1.__lt__) == True
     assert (s1 > s2) == True
     assert (s1 < s2) == False
-    assert (s1-s2) == 10.0
-
-    # regex_string = r'[\w,\W]*Elapsed'
-    # regex_string += r'[\w,\W]*time'
-    # regex_string += r'[\w,\W]*'
-    # print(regex_string)
-    # res = re.search(regex_string, lines[1])
-    # assert res != None
-    # print(res.group())
+    print(f'Difference between s1 and s2 scores: {s1-s2}')
+    assert (s1 - s2) == 10.0
